6db0bf03a3981badef76aab8743701c8200f5444
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2020-05-25 14:22:43 -0300

    Versión 1.0 25/05/2020

---
+++ Quiniela/Form1.Designer.vb
@@ -0,0 +1,170 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.nudNumero = New System.Windows.Forms.NumericUpDown()
+        Me.btnAgregar = New System.Windows.Forms.Button()
+        Me.lboxApostados = New System.Windows.Forms.ListBox()
+        Me.btnQuitar = New System.Windows.Forms.Button()
+        Me.btnVaciar = New System.Windows.Forms.Button()
+        Me.lbxSorteados = New System.Windows.Forms.ListBox()
+        Me.btnSortear = New System.Windows.Forms.Button()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        CType(Me.nudNumero, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 21)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(69, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nro. Apostar:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 69)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(75, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "N° Apostados:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(277, 69)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(73, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "N° Sorteados:"
+        '
+        'nudNumero
+        '
+        Me.nudNumero.Location = New System.Drawing.Point(87, 19)
+        Me.nudNumero.Maximum = New Decimal(New Integer() {99, 0, 0, 0})
+        Me.nudNumero.Name = "nudNumero"
+        Me.nudNumero.Size = New System.Drawing.Size(48, 20)
+        Me.nudNumero.TabIndex = 3
+        '
+        'btnAgregar
+        '
+        Me.btnAgregar.Location = New System.Drawing.Point(141, 16)
+        Me.btnAgregar.Name = "btnAgregar"
+        Me.btnAgregar.Size = New System.Drawing.Size(75, 23)
+        Me.btnAgregar.TabIndex = 4
+        Me.btnAgregar.Text = "Agregar"
+        Me.btnAgregar.UseVisualStyleBackColor = True
+        '
+        'lboxApostados
+        '
+        Me.lboxApostados.FormattingEnabled = True
+        Me.lboxApostados.Location = New System.Drawing.Point(15, 85)
+        Me.lboxApostados.Name = "lboxApostados"
+        Me.lboxApostados.Size = New System.Drawing.Size(120, 290)
+        Me.lboxApostados.TabIndex = 5
+        '
+        'btnQuitar
+        '
+        Me.btnQuitar.Location = New System.Drawing.Point(141, 85)
+        Me.btnQuitar.Name = "btnQuitar"
+        Me.btnQuitar.Size = New System.Drawing.Size(75, 23)
+        Me.btnQuitar.TabIndex = 6
+        Me.btnQuitar.Text = "Quitar"
+        Me.btnQuitar.UseVisualStyleBackColor = True
+        '
+        'btnVaciar
+        '
+        Me.btnVaciar.Location = New System.Drawing.Point(141, 114)
+        Me.btnVaciar.Name = "btnVaciar"
+        Me.btnVaciar.Size = New System.Drawing.Size(75, 23)
+        Me.btnVaciar.TabIndex = 7
+        Me.btnVaciar.Text = "Vaciar"
+        Me.btnVaciar.UseVisualStyleBackColor = True
+        '
+        'lbxSorteados
+        '
+        Me.lbxSorteados.FormattingEnabled = True
+        Me.lbxSorteados.Location = New System.Drawing.Point(280, 85)
+        Me.lbxSorteados.Name = "lbxSorteados"
+        Me.lbxSorteados.Size = New System.Drawing.Size(126, 290)
+        Me.lbxSorteados.TabIndex = 8
+        '
+        'btnSortear
+        '
+        Me.btnSortear.Location = New System.Drawing.Point(320, 16)
+        Me.btnSortear.Name = "btnSortear"
+        Me.btnSortear.Size = New System.Drawing.Size(86, 23)
+        Me.btnSortear.TabIndex = 9
+        Me.btnSortear.Text = "Ver Sorteados"
+        Me.btnSortear.UseVisualStyleBackColor = True
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Location = New System.Drawing.Point(331, 392)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(75, 23)
+        Me.btnSalir.TabIndex = 10
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(428, 418)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.btnSortear)
+        Me.Controls.Add(Me.lbxSorteados)
+        Me.Controls.Add(Me.btnVaciar)
+        Me.Controls.Add(Me.btnQuitar)
+        Me.Controls.Add(Me.lboxApostados)
+        Me.Controls.Add(Me.btnAgregar)
+        Me.Controls.Add(Me.nudNumero)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Quiniela"
+        CType(Me.nudNumero, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents nudNumero As NumericUpDown
+    Friend WithEvents btnAgregar As Button
+    Friend WithEvents lboxApostados As ListBox
+    Friend WithEvents btnQuitar As Button
+    Friend WithEvents btnVaciar As Button
+    Friend WithEvents lbxSorteados As ListBox
+    Friend WithEvents btnSortear As Button
+    Friend WithEvents btnSalir As Button
+End Class
---
+++ Quiniela/Form1.vb
@@ -0,0 +1,155 @@
+﻿Public Class Form1
+    Private arrayApostados() As Integer
+    Dim indice_ArrayApostados As Integer = 0
+    Private arraySorteados(20) As Integer
+
+    Private Sub btnAgregar_Click(sender As Object, e As EventArgs) Handles btnAgregar.Click
+        Try
+            If indice_ArrayApostados = 0 Then
+                If lboxApostados.Items.Count < 20 Then
+                    lboxApostados.Items.Add(nudNumero.Value)
+                    lboxApostados.SelectedIndex = 0 ' para que se quede seleccionado el primer item de la lista.
+                    ReDim Preserve arrayApostados(indice_ArrayApostados + 1)
+                    arrayApostados(indice_ArrayApostados) = nudNumero.Value
+                    indice_ArrayApostados += 1
+                Else
+                    MsgBox("No puede apostar mas de 20 numeros", vbExclamation, "Error")
+                End If
+            Else
+                ' indiceArray distinto de cero
+                If lboxApostados.Items.Count < 20 Then
+                    If busquedaApostados(nudNumero.Value) Then
+                        MsgBox("Numero ya apostado", vbExclamation, "Aviso")
+                    Else
+                        lboxApostados.Items.Add(nudNumero.Value)
+                        lboxApostados.SelectedIndex = 0 ' para que se quede seleccionado el primer item de la lista.
+                        ReDim Preserve arrayApostados(indice_ArrayApostados + 1)
+                        arrayApostados(indice_ArrayApostados) = nudNumero.Value
+                        indice_ArrayApostados += 1
+                        ' Nota: Length será un valor superior al último index del Array
+                    End If
+                Else
+                    MsgBox("No puede apostar mas de 20 numeros", vbExclamation, "Error")
+                End If
+
+            End If
+        Catch ex As Exception
+            MsgBox("Error fué: " & ex.Message, vbCritical, "Se produjo un error al intentar realizar la acción")
+        End Try
+
+    End Sub
+
+    Private Sub btnQuitar_Click(sender As Object, e As EventArgs) Handles btnQuitar.Click
+        Try
+            ' eliminar elemento del Array de apostados (Logica):
+            Dim arrayAuxiliar(arrayApostados.Length - 1) As Integer
+            Dim elem_borrar As Integer = CInt(lboxApostados.SelectedItem)
+            Dim indexNuevoArray As Integer = 0
+
+            For Each elem As Integer In arrayApostados
+                If elem <> elem_borrar Then ' elemento que se preservará en el array de apostados.
+                    arrayAuxiliar(indexNuevoArray) = elem
+                    indexNuevoArray += 1
+                End If
+            Next
+            Erase arrayApostados
+            arrayApostados = arrayAuxiliar
+            indice_ArrayApostados -= 1 ' se le resta 1 ya que en este procedimiento se da de baja a un solo elemento
+            Erase arrayAuxiliar ' vaciamos arrayAuiliar para su posterior uso.
+
+            ' eliminar elemento del ListBox (GUI):
+            lboxApostados.Items.Remove(lboxApostados.SelectedItem)
+            If lboxApostados.Items.Count <> 0 Then
+                lboxApostados.SelectedIndex = 0
+            End If
+        Catch ex As Exception
+            MsgBox("Error fué: " & ex.Message, vbCritical, "Se produjo un error al intentar realizar la acción")
+        End Try
+
+    End Sub
+
+    Private Sub btnVaciar_Click(sender As Object, e As EventArgs) Handles btnVaciar.Click
+        lboxApostados.Items.Clear()
+        Erase arrayApostados ' vaciar Array
+
+    End Sub
+
+    Private Sub btnSortear_Click(sender As Object, e As EventArgs) Handles btnSortear.Click
+        Try     '<>
+            lbxSorteados.Items.Clear()
+            ReDim arraySorteados(20) ' con esto vacio los elementos del array (para evitar problemas)
+            ' nota: el ReDim es utilizado para cambiar tamaño de array sin preservar sus elementos.
+            Dim numero As New Random
+            Dim x As Integer
+            Dim cont As Integer = 0 'contador
+            Do
+                x = Math.Truncate(numero.NextDouble * 999)
+                If busquedaSorteados(x) = False Then
+                    arraySorteados(cont) = x
+                    cont += 1
+                End If
+            Loop While cont < 20
+            Dim i As Integer = 0
+            For i = 0 To 19 Step 1 ' mostrar los elementos sorteados en el ListBox 
+                lbxSorteados.Items.Add(arraySorteados(i))
+            Next
+
+            verAciertos()
+
+        Catch ex As Exception
+            MsgBox(ex.Message, vbCritical, "Error")
+        End Try
+    End Sub
+
+    Private Function busquedaSorteados(ByRef x As Integer) As Boolean
+        ' metodo que busca si el numero sorteado pertenece al array
+        ' caso que pertenezca retorna true, caso contrario false
+        Dim pertenece As Boolean = False
+        Dim i As Integer = 0
+        For i = 0 To 19 Step 1
+            If arraySorteados(i) = x Then
+                pertenece = True
+                Exit For ' en caso que sea afirmativo salimos, no es necesario seguir comprobando.
+            End If
+        Next
+        Return pertenece
+    End Function
+
+    Private Function busquedaApostados(ByRef x As Integer) As Boolean
+        ' metodo que busca si el numero sorteado pertenece al arrayApostados
+        ' caso que pertenezca retorna true, caso contrario false
+        Dim pertenece As Boolean = False
+        For i As Integer = 0 To indice_ArrayApostados Step 1
+            If arrayApostados(i) = x Then
+                pertenece = True
+                Exit For
+            End If
+        Next
+        Return pertenece
+    End Function
+
+    Private Sub verAciertos()
+
+        'mostramos en MsgBox'es los numeros acertados.:
+        Try
+            For i As Integer = 0 To 19 Step 1
+                For j As Integer = 0 To (indice_ArrayApostados) Step 1
+                    If arraySorteados(i) = arrayApostados(j) Then
+                        MsgBox("Número: " & arraySorteados(i), vbInformation, "Número acertado")
+                    End If
+                Next
+            Next
+        Catch ex As Exception
+            MsgBox("Se produjo el siguiente error: " & ex.Message, vbCritical, "Error")
+        End Try
+
+    End Sub
+
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        Dim i As Integer = MsgBox("¿Estás seguro que deseas cerrar el programa?", vbQuestion + vbYesNo, "Confirmar salida")
+        If i = 6 Then
+            Me.Close()
+        End If
+    End Sub
+
+End Class
